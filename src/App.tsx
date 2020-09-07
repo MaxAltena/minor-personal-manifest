6ddeb40bae9dfbd6f87d5df7507bdf0a1b6b3f25
Max Altena <Max@MaxAltena.com>
2020-09-07 15:11:26 +0200

    :tada: Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from 'styled-components';
 
-function App() {
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body {
+    --monaco-monospace-font: 'Fira Code', 'SF Mono', Monaco, Menlo, Consolas, Courier, 'Coerier New', monospace;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe WPC', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyles />
+      <div className="App">
+        personal manifesto
+
+        {/*
+          Get paid for what you are worth rather than of who you are
+          Produce more than you consume
+          Gain experience rather than wealth
+          There is no failure except in no longer trying
+          Nothing ventured, nothing gained
+          Stick to your values
+          Key aspects should never change
+          Opportunities don't happen. You create them.
+          Learn from your mistakes
+          Work together, play together
+          Offer servers that difference
+          Give honest and timely feedback
+          Failure is not a waste of time
+          People over profits
+        */}
+      </div>
+    </>
   );
 }
 
