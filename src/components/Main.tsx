b63a40180e579b499407c5b5aa9afd27eb280065
Max Altena <Max@MaxAltena.com>
2020-09-13 20:59:27 +0200

    :sparkles: v1 of my Personal Manifesto

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,30 +6,37 @@ import { Tabs } from "./";
 const Main = () => {
 	const defaultValue = `{
 	"name": "Max Altena",
+	"description": "Young man trying to get his degree and grow a successful business",
 	"age": 22,
-	"dob": "1998-09-10T00:00:00.000Z",
+	"dob": "1998-09-10T09:30:00.000Z",
 	"email": "max@maxaltena.com",
 	"lastEdited": "${new Date().toISOString()}",
 	"values": {
-		"Get paid for what you are worth rather than of who you are": "",
-		"Produce more than you consume": "",
-		"Gain experience rather than wealth": "",
-		"There is no failure except in no longer trying": "",
-		"Nothing ventured, nothing gained": "",
-		"Stick to your values": "",
-		"Key aspects should never change": "",
-		"Opportunities don't happen. You create them.": "",
-		"Learn from your mistakes": "",
-		"Work together, play together": "",
-		"Offer services that differentiate": "",
-		"failure": "!= a waste of time",
-		"people": "> profits",
 		"give feedback": [
 			"timely",
 			"honest",
 			"constructive"
 		],
-	}
+		"values": [
+			["failure", "!=", "a waste of time"],
+			["people", ">", "profits"],
+			["work together", "===", "play together"]
+		]
+	},
+	"standards": [
+		"Produce more than you consume",
+		"Gain experience rather than wealth",
+		"Get paid for what you are worth rather than of who you are",
+		"Learn from your mistakes",
+		"Offer services that differentiate",
+		"Stick to your values"
+	],
+	"relatableQoutes": [
+		"There is no failure except in no longer trying.",
+		"Nothing ventured, nothing gained",
+		"Opportunities don't happen. You create them.",
+		"Nothing is wrong," "except this"
+	]
 }`;
 
 	const editorDidMount = (editor: any) => {
@@ -40,14 +47,15 @@ const Main = () => {
 		<Container>
 			<Tabs />
 			<Editor>
-				<MonacoEditor theme="vs-dark" language="json" defaultValue={defaultValue} options={{
+				<MonacoEditor theme="vs-dark" language="json" defaultValue={defaultValue} width="100%" height="calc(100% - 35px)" options={{
 					fontLigatures: true,
 					fontFamily: `'Fira Code', 'SF Mono', Monaco, Menlo, Consolas, Courier, 'Coerier New', monospace`,
-					fontSize: 20,
+					fontSize: 18,
 					tabSize: 2,
 					selectOnLineNumbers: true,
 					renderWhitespace: "all",
-					autoIndent: "full"
+					autoIndent: "full",
+					automaticLayout: true
 				}} editorDidMount={editorDidMount} />
 			</Editor>
 		</Container>
